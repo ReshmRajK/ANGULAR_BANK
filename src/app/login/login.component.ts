f4ae42b71e4a380206f3ea4a5ab6d53b50c5b624
ReshmaRajK <reshmarajmsm@gmail.com>
2023-07-13 10:42:38 +0530

    event binding using template rendering variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,8 +9,22 @@ export class LoginComponent {
 
   data="Happy Banking With Us...!Welcome To Net Banking With SBI"
   pdata="Enter Account Number"
+  acno:string=""
+  psw:string=""
 
-  login(){
-    alert("Login Clicked")
+  login(a:any,b:any){
+    // alert("Login Clicked")
+   
+    this.acno=a.value
+    this.psw=b.value
+    console.log(this.acno);
+    console.log(this.psw); 
   }
+  // acnoChange(event:any){
+  //   console.log(event.target.value);
+  // }
+  // pswdChange(event:any){
+  //   console.log(event.target.value);
+    
+  // }
 }
