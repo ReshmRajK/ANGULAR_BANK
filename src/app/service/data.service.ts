3158a3bbb06aa856882146a36d7305e355eb1e6f
ReshmaRajK <reshmarajmsm@gmail.com>
2023-07-14 10:55:09 +0530

    data and method of service file added

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  sData="data inside service file"
+
+  constructor() { }
+  accessData(data:any){
+    console.log(data);
+    
+  }
+}
